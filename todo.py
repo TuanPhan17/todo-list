3104de52551f5fef38392c7ad00f26c0e12e36c3
TuanPhan17 <t_phan17@yahoo.com>
2025-09-25 16:18:41 -0700

    step3: add_task feature (clean single main loop)

--- todo.py
+++ todo.py
@@ -18,9 +18,6 @@ def main():
         else:
             print("Not implemented yet.\n")
 
-if __name__ == "__main__":
-    main()
-
 # TO-DO List Step 2 (list tasks)
 
 tasks = [] # we'll store dicts later; for now just strings is fine 
@@ -48,6 +45,49 @@ def main():
         choice = input("Choose: ").strip()
         if choice == "1":
             list_task()
+        elif choice == "5":
+            print("Bye!")
+            break
+        else:
+            print("Not implemented yet.\n")
+
+#To-Do List Step 3 (add tasks)
+
+tasks = []
+
+def list_tasks():
+    if not tasks:
+        print("\nNo tasks yet.\n")
+        return
+    print("\nYour Tasks:")
+    for i, t in enumerate(tasks, start=1):
+        print(f"{i}. {t}")
+    print()
+
+def add_task():
+    title = input("New task: ").strip()
+    if title:
+        tasks.append(title)
+        print("Added!\n")
+    else:
+        print("Empty task ignored.\n")
+
+def menu():
+    print("== TO-DO LIST ==")
+    print("1) List tasks")
+    print("2) Add task")
+    print("3) Complete task")
+    print("4) Delete task")
+    print("5) Quit")
+
+def main():
+    while True:
+        menu()
+        choice = input("Choose: ").strip()
+        if choice == "1":
+            list_tasks()
+        elif choice == "2":
+            add_task()
         elif choice == "5":
             print("Bye!")
             break
