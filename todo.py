7114c3202a9320284fc7372aaaa09643ebdffb8e
TuanPhan17 <t_phan17@yahoo.com>
2025-09-25 15:39:40 -0700

    step2: list_tasks and tasks store

--- todo.py
+++ todo.py
@@ -20,3 +20,39 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# TO-DO List Step 2 (list tasks)
+
+tasks = [] # we'll store dicts later; for now just strings is fine 
+
+def list_task():
+    if not tasks:
+        print("\nNo tasks yet.\n")
+        return
+    print("\nYour Tasks")
+    for i, t in enumerate(tasks, start=1):
+        print(f"{i}. {t}")
+    print()
+
+def menu():
+    print("== TO-DO LIST ==")
+    print("1) List tasks")
+    print("2) Add task")
+    print("3) Complete task")
+    print("4) Delete task")
+    print("5) Quit")
+
+def main():
+    while True:
+        menu()
+        choice = input("Choose: ").strip()
+        if choice == "1":
+            list_task()
+        elif choice == "5":
+            print("Bye!")
+            break
+        else:
+            print("Not implemented yet.\n")
+
+if __name__ == "__main__":
+    main()
